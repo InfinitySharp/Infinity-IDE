fd3cfed06b702902e59b1de671da62fa27c091c7
Infinity Sharp <creepy.tv02@gmail.com>
2017-05-05 22:29:15 -0400

    Smart Screen will open. 'more & run anyway'.

---
+++ Infinity IDE.bat
@@ -0,0 +1,17 @@
+﻿@echo off
+title Infinity IDE
+color 0a
+:main
+@echo off
+cls
+echo Infinity IDE v1.0
+echo.
+set /p fileName="Name of file: "
+goto edit
+
+:edit
+@echo off
+cls
+set /p lineOne=": "
+echo %lineOne% >> %fileName%
+goto edit
